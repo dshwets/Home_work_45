82f0fdf34bd92db0042226048b9de81675978cbc
Daniil <dshwets@gmail.com>
2020-07-24 22:31:27 +0600

    Добавленно поле, сделаны миграции, создана фикстура.

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -6,6 +6,9 @@ class TO_DO_List(models.Model):
     description = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Описание')
     status = models.CharField(max_length=40, null=False, blank=False, default='new', verbose_name='Статус')
     deadline = models.DateField(default=None, blank=True, null=True, verbose_name='Дата выполнения', choices=STATUS_CHOICES)
+    long_description = models.TextField(max_length=3000, null=True, blank=True, verbose_name='Описание',
+                                        default=None)
+
     def __str__(self):
 
         return "{}. {}".format(self.pk, self.description)
