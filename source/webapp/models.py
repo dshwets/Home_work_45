9227cb6f0c044e2263043c7b92679b7eb8192f39
Daniil <dshwets@gmail.com>
2020-07-24 22:12:13 +0600

    Добавил choices в модель.

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,11 +1,11 @@
 from django.db import models
+STATUS_CHOICES = [('new', 'Новая'), ('in_progress', 'В процессе'), ('done', 'Сделано')]
 
 
 class TO_DO_List(models.Model):
     description = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Описание')
     status = models.CharField(max_length=40, null=False, blank=False, default='new', verbose_name='Статус')
-    deadline = models.DateField(default=None, blank=True, null=True, verbose_name='Дата выполнения')
-
+    deadline = models.DateField(default=None, blank=True, null=True, verbose_name='Дата выполнения', choices=STATUS_CHOICES)
     def __str__(self):
 
         return "{}. {}".format(self.pk, self.description)
