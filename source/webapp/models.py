12a4a0cf538f584b7cdb94547e5a0e38e58eed40
Daniil <dshwets@gmail.com>
2020-08-04 19:09:40 +0600

    Изменил модель

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -4,11 +4,29 @@ STATUS_CHOICES = [('new', 'Новая'), ('in_progress', 'В процессе'),
 
 
 class TO_DO_List(models.Model):
-    description = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Описание')
-    status = models.CharField(max_length=40, null=False, blank=False, default='new', verbose_name='Статус', choices=STATUS_CHOICES)
-    deadline = models.DateField(default=None, blank=True, null=True, verbose_name='Дата выполнения')
+    summary = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Описание')
     long_description = models.TextField(max_length=3000, null=True, blank=True, verbose_name='Описание подробное',
                                         default=None)
+    status = models.ForeignKey('webapp.Statuses', related_name='statuses', on_delete=models.PROTECT,
+                               verbose_name='Статус')
+    issue = models.ForeignKey('webapp.Issues', related_name='issues', on_delete=models.PROTECT,
+                              verbose_name='Тип задачи')
+
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
 
     def __str__(self):
-        return "{}. {}".format(self.pk, self.description)
+        return "{}. {}".format(self.pk, self.summary)
+
+
+class Statuses(models.Model):
+    statuses = models.CharField(max_length=40, null=False, blank=False, verbose_name='Статус')
+
+    def __str__(self):
+        return self.statuses[:20]
+
+
+class Issues(models.Model):
+    issues = models.CharField(max_length=40, null=False, blank=False, default='new', verbose_name='Статус')
+
+    def __str__(self):
+        return self.issues[:20]
