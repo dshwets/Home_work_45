187d084d53d4c68febf16683839fcee01ae8dfaa
Daniil <dshwets@gmail.com>
2020-08-18 21:04:38 +0600

    Изменил вью на ЛистВью, добавил Пагинацию.

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -2,18 +2,23 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponseNotAllowed
 from webapp.models import TO_DO_List
 from webapp.forms import ToDoForm
-from django.views.generic import View, TemplateView, FormView
+from django.views.generic import View, TemplateView, FormView, ListView
 from django.urls import reverse
 
 
 
-class IndexView(TemplateView):
+class IndexView(ListView):
     template_name = 'index.html'
+    model = TO_DO_List
+    context_object_name = 'to_do_list'
+    ordering = ['-created_at']
+    paginate_by = 2
+    paginate_orphans = 1
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['to_do_list'] = TO_DO_List.objects.all()
-        return context
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['to_do_list'] = TO_DO_List.objects.all()
+    #     return context
 
 
 class DeleteTodoView(View):
