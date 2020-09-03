b6155617c88fcda837b68eaf1564bf9671e30d3e
Daniil <dshwets@gmail.com>
2020-09-04 01:34:03 +0600

    Сделал дз, пока без бонусов

---
+++ source/accounts/forms.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.forms import forms, EmailField, CharField
+
+
+class MyUserCreationForm(UserCreationForm):
+    email = EmailField(required=True, max_length=150)
+
+    class Meta(UserCreationForm.Meta):
+        fields = ['username', 'password1', 'password2',
+                  'first_name', 'last_name', 'email']
+        # widgets = {
+        #     'email': EmailField(required=True, max_length=None)
+        # }
+
+    def clean(self):
+        cleaned_data = super(UserCreationForm, self).clean()
+        first_name = cleaned_data.get("first_name")
+        last_name = cleaned_data.get("last_name")
+
+        if not first_name or not last_name:
+            raise forms.ValidationError(
+                "You should fill one of the fields First name or Last name",
+                code='Incorrect First_name or Last_name'
+            )
--- source/main/urls.py
+++ source/main/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from webapp.views import IndexView,CreateTodoView, DeleteTodoView, WatchTodoView, UpdateTodoView, Project_view, \
     Watch_project_view, Create_project_view,ProjectToDoCreateView, ProjectUpdateView, ProjectDeleteView
 from django.contrib.auth.views import LogoutView, LoginView
@@ -34,7 +34,6 @@ urlpatterns = [
     path('projects/<int:pk>/update/', ProjectUpdateView.as_view(), name='update_project'),
     path('projects/<int:pk>/delete/', ProjectDeleteView.as_view(), name='delete_project'),
 
-    path('accounts/login/', LoginView.as_view(), name='login'),
-    path('accounts/logout', LogoutView.as_view(), name='logout')
+    path('accounts/', include('accounts.urls')),
 
 ]
