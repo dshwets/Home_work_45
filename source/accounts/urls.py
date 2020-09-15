ac1da04b63a607d106c26fe7c52ad46fab778814
Daniil <dshwets@gmail.com>
2020-09-15 23:20:08 +0600

    Дз сделано )

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from accounts.views import RegisterView, UserDetailView, WatchUsersView
+from accounts.views import RegisterView, UserDetailView, WatchUsersView, UserChangeView, UserPasswordChangeView
 
 app_name = 'accounts'
 
@@ -9,5 +9,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('create/', RegisterView.as_view(), name='create'),
     path('<int:pk>/', UserDetailView.as_view(), name='detail'),
-    path('users_list/', WatchUsersView.as_view(), name='users_list')
+    path('users_list/', WatchUsersView.as_view(), name='users_list'),
+    path('<int:pk>/update/', UserChangeView.as_view(), name='change'),
+    path('<int:pk>/password-change', UserPasswordChangeView.as_view(), name='password_change')
 ]
--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,13 +1,14 @@
 from django.contrib import auth
 from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
-from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
 from django.contrib.auth import get_user_model
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin, UserPassesTestMixin
 
-from accounts.forms import MyUserCreationForm
+from accounts.forms import MyUserCreationForm, UserChangeForm, ProfileChangeForm, PasswordChangeForm
 from accounts.models import Profile
 
 
@@ -52,3 +53,59 @@ class WatchUsersView(PermissionRequiredMixin, ListView):
     model = Profile
     context_object_name = 'profiles'
     permission_required = 'accounts.can_watch_users'
+
+
+class UserChangeView(UserPassesTestMixin, UpdateView):
+    model = get_user_model()
+    form_class = UserChangeForm
+    template_name = 'user_change.html'
+    context_object_name = 'user_obj'
+
+    def get_context_data(self, **kwargs):
+        if 'profile_form' not in kwargs:
+            kwargs['profile_form'] = self.get_profile_form()
+        return super().get_context_data(**kwargs)
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        profile_form = self.get_profile_form()
+        if form.is_valid() and profile_form.is_valid():
+            return self.form_valid(form, profile_form)
+        else:
+            return self.form_invalid(form, profile_form)
+
+    def form_valid(self, form, profile_form):
+        form.save()
+        profile_form.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+    def form_invalid(self, form, profile_form):
+        context = self.get_context_data(form=form, profile_form=profile_form)
+        return self.render_to_response(context)
+
+    def get_success_url(self):
+        return reverse('accounts:detail', kwargs={'pk': self.object.pk})
+
+    def get_profile_form(self):
+        form_kwargs = {'instance': self.object.profile}
+        if self.request.method == 'POST':
+            form_kwargs['data'] = self.request.POST
+            form_kwargs['files'] = self.request.FILES
+        return ProfileChangeForm(**form_kwargs)
+
+    def test_func(self):
+        return self.kwargs['pk'] == self.request.user.pk
+
+
+class UserPasswordChangeView(UserPassesTestMixin, UpdateView):
+    model = get_user_model()
+    template_name = 'user_password_change.html'
+    form_class = PasswordChangeForm
+    context_object_name = 'user_obj'
+
+    def get_success_url(self):
+        return reverse('accounts:login')
+
+    def test_func(self):
+        return self.kwargs['pk'] == self.request.user.pk
